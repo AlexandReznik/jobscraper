6ad3be54efbb270632893ee5152be9a970985099
AlexandReznik <saha.reznik2004@gmail.com>
2024-07-08 17:05:10 +0200

    Deleted commented code and added env. variables to ci/cd pipline

--- app/common/database.py
+++ app/common/database.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import sessionmaker
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 
                                                     '../../app/app.db')}"
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./app/app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
--- run_spiders.py
+++ run_spiders.py
@@ -10,24 +10,3 @@ process.crawl(DjinniSpider)
 process.crawl(DouSpider)
 process.crawl(LhhSpider)
 process.start()
-# from scrapy.crawler import CrawlerProcess
-# from .app.job_scraper.spiders.djinni import DjinniSpider
-# from .app.job_scraper.spiders.dou import DouSpider
-# from .app.job_scraper.spiders.lhh import LhhSpider
-# from .app.job_scraper import settings as scraper_settings
-
-
-# def run_scrapers():
-#     process = CrawlerProcess(settings={
-#         **scraper_settings,
-#         'LOG_LEVEL': 'INFO',
-#     })
-    
-#     process.crawl(DjinniSpider)
-#     process.crawl(DouSpider)
-#     process.crawl(LhhSpider)
-#     process.start()
-
-
-# if __name__ == '__main__':
-#     run_scrapers()
