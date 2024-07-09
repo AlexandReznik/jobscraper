ba944f830c17dd5ef5984a6412f1cf800838258e
AlexandReznik <saha.reznik2004@gmail.com>
2024-07-09 17:40:39 +0200

    Created module that will support the functionality of sending email messages

---
+++ app/common/email_utils.py
@@ -0,0 +1,27 @@
+import smtplib
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def send_email(subject, body, to_email):
+    from_email = os.getenv("FROM_EMAIL")
+    from_password = os.getenv("FROM_PASSWORD")
+    smtp_server = os.getenv("SMTP_SERVER")
+    smtp_port = os.getenv("SMTP_PORT")
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(from_email, from_password)
+    server.sendmail(from_email, to_email, msg.as_string())
+    server.quit()
